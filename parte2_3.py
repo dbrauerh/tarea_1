2ffe4d54d1d49f74b87b3a5ed2c16452b823483e
Daniel <40126100+dbrauerh@users.noreply.github.com>
2018-09-28 22:01:29 -0300

    pregunta 2 parte 3 1

---
+++ parte2_3.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mp 
+import math
+archivo=open("columnas.txt","r")
+columna1=[]
+columna2=[]
+a=archivo.readlines()
+for linea in a:
+    columna1.append((300e8)*float(linea[1:5]))
+    columna2.append(float(linea[9:15]))
+
+area=0
+n=0
+while n < (len(columna2)-1):
+    delta=columna1[n+1]-columna1[n]
+    area+=delta*(columna2[n]+columna2[n+1])/2.0
+    n+=1
+
+print(area)
